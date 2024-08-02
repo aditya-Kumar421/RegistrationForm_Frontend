e3d16da07711c3b1abf6adcd28a1dbbc51bf6ac3
Aditya <ak.adityakumar421@gmail.com>
2024-08-02 11:42:15 +0530

    add condition in success

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SuccessPage = () => {
-  return <center className='thanks-container'>
-    <img className="thanksImg" src="../src/assets/ThankYouImg.webp" alt="Thanks" />
-  </center>;
+  const [imageLoaded, setImageLoaded] = useState(true);
+
+  const handleImageError = () => {
+    setImageLoaded(false);
+  };
+
+  return (
+    <center className='thanks-container'>
+      {imageLoaded ? (
+        <img 
+          className="thanksImg" 
+          src="../src/assets/ThankYouImg.webp" 
+          alt="Thanks" 
+          onError={handleImageError}
+        />
+      ) : (
+        <h1>Thank You For Registering!</h1>
+      )}
+    </center>
+  );
 };
 
 export default SuccessPage;
